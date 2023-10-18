c5541bfbd0ba222c250985c3dfad09223228c0bd
Shahbaz Ahmad Siddiqui <70898307+shahbazaldiablo@users.noreply.github.com>
2023-10-19 04:57:06 +0530

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import "./app.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
 };
 
 export default App;
