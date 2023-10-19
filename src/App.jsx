88e8d5bed12bd990c566cfd457f96ef10c7f79ee
Shahbaz Ahmad Siddiqui <70898307+shahbazaldiablo@users.noreply.github.com>
2023-10-19 05:54:34 +0530

    newer files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <section>Hero</section>
+      <section>
+        <Navbar />
+      </section>
       <section>Parallax</section>
       <section>Services</section>
       <section>Parallax</section>
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+import "./sidebar.scss";
+
+const Sidebar = () => {
+  return <div className="sidebar"></div>;
+};
+
+export default Sidebar;
