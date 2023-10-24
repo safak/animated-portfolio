f6d4b88124fbf61d50f128d6b2596fd1957bd55a
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-10-24 22:14:58 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div className="element">
+    <section>
+      <Navbar />
+    </section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>;
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+return (
+    <div className="navbar">
+        {/* Sidebar */}
+        <div className="wrapper">
+            <span>Digital Navas</span>
+            <div className="social">
+                <a href="#">
+                    <img src="" alt="" />
+                </a>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default Navbar
