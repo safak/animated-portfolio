516ed8daaafd94d999708628dd97354aae51ed7a
robydinca <robertmdinca@proton.me>
2023-10-26 23:44:17 +0200

    menu animado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.css";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section >
+      <Navbar />
+    </section>
+    <section id="services">Parallax</section>
+    <section>Services</section>
+    <section id="portfolio">Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="contact">Contact</section>
+  </div>;
 };
 
 export default App;
