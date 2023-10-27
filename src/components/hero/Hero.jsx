92a3f4d931d3276bf7588b70258896b082fc3c2e
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-10-27 22:26:17 +0200

    animation-hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,18 +1,52 @@
 import "./hero.scss"
+import { motion } from "framer-motion"
+
+const textVariant = {
+    initial: {
+        x: -500,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    },
+    scrollButton: {
+        opacity: 0.2,
+        y: 10,
+        transition: {
+            duration: 2,
+            repeat: Infinity
+        }
+    }
+}
 
 const Hero = () => {
   return (
     <div className='hero'>
         <div className="wrapper">
-            <div className="textContainer">
-                <h2>Gianfranco Navas F</h2>
-                <h1>Web developer</h1>
-                <div className="buttons">
-                    <button>Latest Works</button>
-                    <button>Contact Me</button>
-                </div>
-                <img src="/scroll.png" alt="" />
-            </div>
+            <motion.div 
+                className="textContainer" 
+                variants={textVariant}
+                initial= "initial"
+                animate= "animate"
+                >
+                <motion.h2 variants={textVariant}>Gianfranco Navas F</motion.h2>
+                <motion.h1 variants={textVariant}>Web developer</motion.h1>
+                <motion.div variants={textVariant} className="buttons">
+                    <motion.button variants={textVariant}>Latest Works</motion.button>
+                    <motion.button variants={textVariant}>Contact Me</motion.button>
+                </motion.div>
+                <motion.img 
+                    variants={textVariant}
+                    animate="scrollButton"
+                    src="/scroll.png" 
+                    alt="" 
+                />
+            </motion.div>
         </div>
         <div className="slidingTextContainer">
             Digital Navas
