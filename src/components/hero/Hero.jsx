9d65adb58cd80426fe08ae09394e0f347a05ce7f
Oluwaseun Oyagbile <oyagbileoluwaseun@gmail.com>
2023-11-19 21:58:17 +0100

    on branch starter

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -47,9 +47,9 @@ const Hero = () => {
           initial="initial"
           animate="animate"
         >
-          <motion.h2 variants={textVariants}>HARVEY TYLER</motion.h2>
+          <motion.h2 variants={textVariants}>Dev Name</motion.h2>
           <motion.h1 variants={textVariants}>
-            Web developer and UI designer
+            Web developer, UI designer and Project Mananger
           </motion.h1>
           <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants}>
