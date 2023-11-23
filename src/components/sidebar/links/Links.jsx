4a9c53e56daf535e0035ed0637dcf25ccbeab104
SlumpG <shlomo.gangul1@gmail.com>
2023-11-23 16:41:11 +0200

    sidebar animation

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,13 +1,45 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggeredChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggeredChildren: 0.05,
+      staggeredDirection: -1,
+    },
+  },
+};
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 const Links = () => {
   const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
