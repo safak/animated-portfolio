59dc53b621104c574606bb26863c8f2f24c5c09b
Diwakar <diwakar@gmail.com>
2023-12-11 11:42:25 +0530

    created portfolio component and added the featured items

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./app.scss"
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 const App = () => {
@@ -15,9 +16,10 @@ const App = () => {
       <section id="Services"><Parallax type="services"/></section>
       <section><Services/></section>
       <section id="Portfolio"><Parallax type="portfolio"/></section>
-      <section>Portfolio1</section>
+      {/* <section>Portfolio1</section>
       <section>Portfolio2</section>
-      <section>Portfolio3</section>
+      <section>Portfolio3</section> */}
+      <Portfolio/>
       <section id="Contact">Contact</section>
       {/* <Test/> */}
 
