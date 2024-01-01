74371647bdf63a86449d1670a5736ad68a637e6b
Zainab Al-Masoodi <zainab_almasoodi@yahoo.com>
2023-12-31 19:25:14 -0500

    porjects added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,27 +6,27 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items =[
     {
         id: 1,
-        title: "React Commerce",
+        title: "Todo-App",
         img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+        desc: "This project consists of PostgreSQL, Express, React, and Node.js technologies to build a Todo full-stack web application!",
       },
       {
         id: 2,
-        title: "Next.js Blog",
+        title: "Book Review Application",
         img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+        desc: "In this project, I created APIs and performed CRUD operations on an Express server using Session and JWT authentication. I used promises with Axios in Node.js created REST API endpoints and tested them using Postman.",
       },
       {
         id: 3,
-        title: "Vanilla JS App",
+        title: "Budget Allocation Application",
         img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+        desc: "React.",
       },
       {
         id: 4,
-        title: "Music App",
+        title: "Python Applications",
         img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+        desc: "• Assignment Application / Flask /n • Emotion Detection Application / Python, AI library /n • Online Course Application / Django /n • Car dealership Application / Cloud App, Python",
       },
 ];
 
