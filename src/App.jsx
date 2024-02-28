5dce181d069f0dcd43bc713469275436f4f29902
Abhishek Varshney <gm6603@myamu.ac.in>
2024-02-28 21:00:10 +0530

    Initializing Porfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import "./app.scss"
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>WELCOME</section>
+    <section>Parallax</section>
+    <section>Skills</section>
+    <section>Parallax</section>
+    <section>Project1</section>
+    <section>Project2</section>
+    <section>Project3</section>
+    <section>Project4</section>
+    <section>Contact</section>
+  </div>;
 };
 
 export default App;
