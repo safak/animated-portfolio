3fb825bd775e1b9049c25260292862fc6f1aafcf
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-03-10 15:12:28 +0530

    wlecome section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar"
 import Welcome from "./components/Welcome/Welcome"
+import Parallax from "./components/Parallax/Parallax";
 const App = () => {
   return <div>
     <section id="Home">
     <Navbar/>
     <Welcome/>
     </section>
-    <section id="Skills"><Prallax type={skills}/></section>
+    <section id="Skills"><Parallax type="skills"/></section>
     <section>Skills</section>
-    <section id="Projects"><Parallax type={projects}/></section>
+    <section id="Projects"><Parallax type="projects"/></section>
     <section>Project1</section>
     <section>Project2</section>
     <section>Project3</section>
