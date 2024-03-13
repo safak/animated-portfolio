31f687fe62de18c9cd74d7cad4d39b30d31cf75c
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-03-13 20:54:33 +0530

    Skills Section Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar"
 import Welcome from "./components/Welcome/Welcome"
-import Parallax from "./components/Parallax/Parallax";
-
+import Parallax from "./components/Parallax/Parallax"
+import Skills from "./components/Skills/Skills"
 const App = () => {
   return <div>
     <section id="Home">
@@ -10,7 +10,7 @@ const App = () => {
     <Welcome/>
     </section>
     <section id="Skills"><Parallax type="skills"/></section>
-    <section>Skills</section>
+    <section><Skills /></section>
     <section id="Projects"><Parallax type="projects"/></section>
     <section>Project1</section>
     <section>Project2</section>
