5b06202d61c9745ef9ec623acb89a9773dcbb141
Zainab Al-Masoodi <118070047+Zainab-Almasoodi@users.noreply.github.com>
2024-03-28 12:40:28 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='/animated-portfolio'>
     <App />
+
+      
     </BrowserRouter>
   </React.StrictMode>,
 )
